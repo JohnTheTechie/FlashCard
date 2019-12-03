42f800cc5ccf67ace8950eaed9b415fb96aad20c
JohnTheTechie <janakiraman3394@gmail.com>
2019-12-03 21:06:00 +0530

    ListAdapter issueFixed

--- app/src/main/java/johnfatso/flashcard/ReviewListActivity.java
+++ app/src/main/java/johnfatso/flashcard/ReviewListActivity.java
@@ -11,14 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class ReviewListActivity extends AppCompatActivity implements DBreader{
+
+    final String LOG_TAG = "TAG";
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
@@ -28,6 +27,9 @@ public class ReviewListActivity extends AppCompatActivity implements DBreader{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.v(LOG_TAG, "activity on create entered");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_list);
 
@@ -37,42 +39,49 @@ public class ReviewListActivity extends AppCompatActivity implements DBreader{
 
         repository = new DictEntryRepository(getApplication(), this);
         repository.getDataSet();
+        Log.v(LOG_TAG, "activity on create completed");
     }
 
     public class RecyclerViewCustomAdapter extends RecyclerView.Adapter<RecyclerViewCustomAdapter.CustomViewHolder>{
 
-        public List<DictEntry> dataset;
+        ArrayList<DictEntry> dataset;
 
-        public RecyclerViewCustomAdapter(List<DictEntry> dataset) {
+        RecyclerViewCustomAdapter(ArrayList<DictEntry> dataset) {
             this.dataset = dataset;
+            Log.v(LOG_TAG, "RecyclerViewCustomAdapter created");
         }
 
-        public class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public class CustomViewHolder extends RecyclerView.ViewHolder{
             TextView specimen;
             TextView type;
             TextView translation;
             ImageView deleteButton;
 
-            public CustomViewHolder(@NonNull View itemView, TextView specimen, TextView type, TextView translation, ImageView deleteButton) {
-                super(itemView);
-                this.specimen = specimen;
-                this.type = type;
-                this.translation = translation;
-                this.deleteButton = deleteButton;
+             CustomViewHolder(@NonNull View itemView, TextView specimen, TextView type, TextView translation, ImageView deleteButton) {
+                 super(itemView);
+                 this.specimen = specimen;
+                 this.type = type;
+                 this.translation = translation;
+                 this.deleteButton = deleteButton;
 
-                this.deleteButton.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick(View view) {
-                repository.deleteWord(specimen.getText().toString());
-                removeItemFromTheList(getAdapterPosition());
+                 this.deleteButton.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View view) {
+                         Log.v(LOG_TAG, "delete button clicked");
+                         repository.deleteWord(specimen.getText().toString());
+                         dataset.remove(getAdapterPosition());
+                         adapter.notifyItemRemoved(getAdapterPosition());
+                     }
+                 });
+                 Log.v(LOG_TAG,"CustomViewHolder created");
             }
         }
 
         void removeItemFromTheList(int position){
             Log.v("TAG", "item to be removed :"+position+" | count in list : "+getItemCount());
+
             dataset.remove(position);
+            layoutManager.removeViewAt(position);
             adapter.notifyItemRemoved(position);
             adapter.notifyItemRangeChanged(position, getItemCount());
         }
@@ -81,17 +90,23 @@ public class ReviewListActivity extends AppCompatActivity implements DBreader{
         @Override
         public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+            Log.v(LOG_TAG, "OnCreateViewHolder Entered");
+
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view_review_list_item, parent, false);
             TextView specimen =  view.findViewById(R.id.list_item_specimen);
             TextView type =  view.findViewById(R.id.list_item_word_type);
             TextView translation =  view.findViewById(R.id.list_item_translation);
             ImageView deleteButton = view.findViewById(R.id.deleteEntry_reviewActivity_listEntry);
+
             return new CustomViewHolder(view, specimen, type, translation, deleteButton);
         }
 
 
         @Override
         public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+
+            Log.v(LOG_TAG, "OnBindViewHolder Entered");
+
             holder.specimen.setText(dataset.get(position).getDeutcheWort());
             holder.type.setText(dataset.get(position).getDeutcheWortType());
             holder.translation.setText(dataset.get(position).getEnglischeTranslation());
@@ -104,13 +119,22 @@ public class ReviewListActivity extends AppCompatActivity implements DBreader{
          */
         @Override
         public int getItemCount() {
+            Log.v(LOG_TAG, "getItmCount Entered");
             return dataset.size();
         }
     }
 
     @Override
     public void DBreaderResponse(Object data) {
-        adapter = new RecyclerViewCustomAdapter(Arrays.asList((DictEntry[]) data));
+        adapter = new RecyclerViewCustomAdapter(toArrayList((DictEntry[]) data));
         recyclerView.setAdapter(adapter);
+    }
+
+    ArrayList<DictEntry> toArrayList(DictEntry[] list){
+        ArrayList<DictEntry> arrayList = new ArrayList<>();
+        for( DictEntry item: list){
+            arrayList.add(item);
+        }
+        return arrayList;
     }
 }
